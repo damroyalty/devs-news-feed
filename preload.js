e41ebd780d5cf53b18b3dc45e9b67739889e6c75
Devlin <rewlinofficial@gmail.com>
2025-04-27 18:23:23 -0700

    added search bar

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
+  searchNews: (query) => ipcRenderer.invoke('search-news', query),
   getNews: () => ipcRenderer.invoke('get-news'),
-});;
+});
